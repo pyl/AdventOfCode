98bbc407b2c9262c2f32e95c14c529016e620907
python8u <python8u@gmail.com>
2020-12-02 00:23:23 -0500

    First Commit

---
+++ 1.py
@@ -0,0 +1,14 @@
+f = open("AdventOfCode/input.txt", "r")
+
+firstArray = []
+
+for x in f:
+    firstArray.append(int(x))
+
+
+
+for x in firstArray:
+    for y in firstArray:
+        for z in firstArray:
+            if int(x+y+z) == 2020:
+                print(x*y*z)
